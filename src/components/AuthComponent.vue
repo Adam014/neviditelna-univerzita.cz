63194306411b7c89f56b066b5f0fdc8c741331d0
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-07 01:07:06 +0100

    Added AuthInfo component and Verbose view checkbox

---
+++ src/components/AuthComponent.vue
@@ -0,0 +1,54 @@
+
+
+<template>
+    <section class="auth-container">
+        <h2>
+            AuthInfo: 
+            <span v-if="showPassword" class="secret-password">secretpassword</span>
+            <button @click="togglePasswordVisibility">{{ showPassword ? 'HIDE' : 'SHOW' }}</button>
+        </h2>    
+        <h2 class="expires-h2">Expires At: <span>{{ formattedExpiresAt }}</span></h2>
+    </section>
+</template>
+
+<script>
+
+import domainData from '../utils/domain-detail.json';
+
+export default {
+  data() {
+    return {
+      domainData: domainData,
+      showPassword: false, // Initially, the password is hidden
+    };
+  },
+  computed: {
+    formattedExpiresAt() {
+      const expiresAt = this.domainData.expires_at;
+
+      // Parsing date
+      const expirationDate = new Date(expiresAt);
+
+      // Setting the options for formatting
+      const options = {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric',
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric',
+        hour12: true,
+        timeZone: 'UTC', // timezone to UTC
+      };
+
+      const formatter = new Intl.DateTimeFormat('en-US', options);
+      return formatter.format(expirationDate);
+    },
+  },
+  methods: {
+    togglePasswordVisibility() {
+      this.showPassword = !this.showPassword;
+    },
+  },
+};
+</script>
--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,9 +1,21 @@
 <script setup>
+import AuthComponent from './AuthComponent.vue';
+
 
 </script>
 
 <template>
     <main>
         <h2><bold>neviditelna-univerzita.cz</bold></h2>
+        <div class="verbose-view-container">
+            <div class="cl-toggle-switch">
+                <label class="cl-switch">
+                    <input type="checkbox">
+                    <span></span>
+                </label>
+            </div>
+            <h4>Verbose view</h4>
+        </div>
+        <AuthComponent />
     </main>
 </template>
