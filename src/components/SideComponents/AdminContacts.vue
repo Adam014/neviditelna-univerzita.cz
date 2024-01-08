a7b9b9fdab9f264e12ef869a24306a49dd94d012
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-08 17:57:58 +0100

    Refactored the AdminContactcs because it was a mess..

--- src/components/SideComponents/AdminContacts.vue
+++ src/components/SideComponents/AdminContacts.vue
@@ -1,70 +1,46 @@
 <script setup>
-    import IconEyeTrue from '../icons/IconEyeTrue.vue';
-    import IconEyeClosed from '../icons/IconEyeClosed.vue';
+import IconEyeTrue from '../icons/IconEyeTrue.vue';
+import IconEyeClosed from '../icons/IconEyeClosed.vue';
 </script>
 
 <template>
-    <section class="admin-container">
-        <div class="headline-container">
-        <h2>Administrative contact{{ verboseView === true ? "" : "s" }}:</h2>
-        </div>
-        <div class="admins-information" v-if="!verboseView">
-            <h2 v-for="admin in adminInfo" :key="admin.handle" :data-text="`${admin.name}:`" class="non-verbose">
-                <span class="blue-text">{{ admin.handle }}</span>
-            </h2>
-        </div>
-        <div class="admins-information" v-if="verboseView">
-            <div class="handle-title">
-                <h2 data-text="Handle:"><span class="blue-text">{{ adminInfo[0].handle }}</span></h2>
-            </div>
-            <div class="organization-title">
-                <span v-if="adminInfo[0].publish.organization" class="icon">
-                    <IconEyeTrue />
-                </span>
-                <span v-else class="icon">
-                    <IconEyeClosed />
-                </span>
-                <h2 data-text="Organization:"><span>{{ adminInfo[0].organization }}</span></h2>
-            </div>
-            <div class="name-title">
-                <span v-if="adminInfo[0].publish.organization" class="icon">
-                    <IconEyeTrue />
-                </span>
-                <span v-else class="icon">
-                    <IconEyeClosed />
-                </span>
-                <h2 data-text="Name:"><span>{{ adminInfo[0].name }}</span></h2>
-            </div>
-        </div>
+  <section>
+    <section v-if="!verboseView" class="admin-container">
+      <div class="headline-container">
+        <h2>Administrative contact{{ adminInfo.length > 1 ? 's' : '' }}:</h2>
+      </div>
+      <div class="admins-information">
+        <h2 v-for="admin in adminInfo" :key="admin.handle" :data-text="`${admin.name}:`" class="non-verbose">
+          <span class="blue-text">{{ admin.handle }}</span>
+        </h2>
+      </div>
     </section>
-    <section class="admin-container" v-if="verboseView">
+
+    <section v-else>
+      <section v-for="(admin, index) in adminInfo" :key="index" class="admin-container">
         <div class="headline-container">
             <h2>Administrative contact:</h2>
         </div>
         <div class="admins-information">
-            <div class="handle-title">
-                <h2 data-text="Handle:"><span class="blue-text">{{ adminInfo[1].handle }}</span></h2>
-            </div>
-            <div class="organization-title">
-                <span v-if="adminInfo[1].publish.organization" class="icon">
-                    <IconEyeTrue />
-                </span>
-                <span v-else class="icon">
-                    <IconEyeClosed />
-                </span>
-                <h2 data-text="Organization:"><span>{{ adminInfo[1].organization }}</span></h2>
-            </div>
-            <div class="name-title">
-                <span v-if="adminInfo[1].publish.organization" class="icon">
-                    <IconEyeTrue />
-                </span>
-                <span v-else class="icon">
-                    <IconEyeClosed />
-                </span>
-                <h2 data-text="Name:"><span>{{ adminInfo[1].name }}</span></h2>
-            </div>
+          <div class="handle-title">
+            <h2 data-text="Handle:"><span class="blue-text">{{ admin.handle }}</span></h2>
+          </div>
+          <div class="organization-title">
+            <span :class="['icon', admin.publish.organization ? 'eye-true' : 'eye-closed']">
+              <component :is="admin.publish.organization ? IconEyeTrue : IconEyeClosed" />
+            </span>
+            <h2 data-text="Organization:"><span>{{ admin.organization }}</span></h2>
+          </div>
+          <div class="name-title">
+            <span :class="['icon', admin.publish.organization ? 'eye-true' : 'eye-closed']">
+              <component :is="admin.publish.organization ? IconEyeTrue : IconEyeClosed" />
+            </span>
+            <h2 data-text="Name:"><span>{{ admin.name }}</span></h2>
+          </div>
         </div>
+      </section>
     </section>
+  </section>
 </template>
 
 <script>
@@ -72,7 +48,7 @@ import { getAdminsInfo } from '@/utils/utils';
 
 export default {
   props: {
-    verboseView: Boolean, // Receive verboseView as a prop
+    verboseView: Boolean,
   },
   data() {
     return {
