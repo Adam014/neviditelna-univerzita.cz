f53a4ee82917fdac76d7209053fd80b673d2754c
Adam Stádník <adam.stadnik@seznam.cz>
2024-01-08 18:06:07 +0100

    Deleted unused code

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import Navbar from './components/NavbarComponent.vue'
 import Sidebar from './components/SideBarComponent.vue'
 import Main from './components/MainComponent.vue'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 </script>
 
 <template>
@@ -10,6 +9,5 @@ import { SpeedInsights } from '@vercel/speed-insights/next'
     <Navbar />
     <Sidebar />
     <Main />
-    <SpeedInsights />
   </div>
 </template>
